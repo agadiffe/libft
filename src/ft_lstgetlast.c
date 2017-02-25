0a0be661c71fe3eb3619ae40ffd31f40154aa15a
agadiffe <agadiffe@student.42.fr>
2017-02-25 11:02:13 +0100

    missing include libft.h in lstgetlast

--- src/ft_lstgetlast.c
+++ src/ft_lstgetlast.c
@@ -6,9 +6,16 @@
 /*   By: agadiffe <agadiffe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/25 10:56:13 by agadiffe          #+#    #+#             */
-/*   Updated: 2017/02/25 10:56:29 by agadiffe         ###   ########.fr       */
+/*   Updated: 2017/02/25 11:01:43 by agadiffe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**		DESCRIPTION
+**	Renvoi le dernier element de la liste.
+*/
 
 t_list	*ft_lstgetlast(t_list **alst)
 {
